ad633688eed5951becc69ef332c1033d72acc10a
rutav <33077901+rutav@users.noreply.github.com>
2018-03-23 22:11:52 -0400

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TextAsset asset1;
+    public GameObject tilefab;
+    private string text;
+    public List<GameObject> tiles;
+    public GameObject nospace;
+    void Start()
+    {
+        text = asset1.text;
+        char[,] cArray = new char[8, 8];
+        var lines = text.Split(new[] { '\n' });
+        int row = 0;
+        foreach (string line in lines)
+        {
+            int column = 0;
+            foreach (char character in line)
+            {
+                if (column < 7 & row < 7)
+                {
+                    cArray[row, column] = character;
+                }
+                column++;
+            }
+            row++;
+        }
+        for(int x = 0; x < 7; x++)
+        {
+            for(int y = 0; y <7; y++)
+            {
+                if (cArray[x, y] == '-' || cArray[x, y] == '|')
+                {
+                    Vector2 pos = new Vector2(x,y);
+                    GameObject currentFile = Instantiate(tilefab, pos, Quaternion.identity) as GameObject;
+                    tiles.Add(currentFile);
+                }
+                else
+                {
+                    Vector2 pos1 = new Vector2(x, y);
+                    GameObject currentFile= Instantiate(nospace, pos1, Quaternion.identity) as GameObject;
+                    tiles.Add(currentFile);
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,40 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class NewBehaviourScript1 : MonoBehaviour {
+ 
+     private int[,] spaces;
+ 
+     void Start() {
+         spaces = ParseFile("C:/Users/Rutav/Documents/self trty/Assets/text.txt");
+             // Output the data to verify the read
+         for (int i = 0; i < spaces.GetLength(0); i++) {
+             Debug.Log(spaces[i,0].ToString ()+spaces[i,1].ToString()+spaces[i,2].ToString ()+spaces[i,3].ToString()+spaces[i,4].ToString()+
+                       spaces[i,5].ToString ()+spaces[i,6].ToString()+spaces[i,7].ToString ()+spaces[i,8].ToString());
+         }
+     }
+ 
+     private int[,] ParseFile(string filePath) {
+ 
+         string input = System.IO.File.ReadAllText (filePath);
+         string[] lines = input.Split (new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+         int[,] spaces = new int[lines.Length, 9];
+ 
+         for (int i = 0; i < lines.Length; i++) {
+             string st = lines[i];
+             string[] nums = st.Split(new[] { ',' });
+             if (nums.Length != 9) {
+                 Debug.Log ("Misforned input on line "+i+1);
+             }
+             for (int j = 0; j < Mathf.Min (nums.Length, 9); j++) {
+                 int val;
+                 if (int.TryParse (nums[j], out val))
+                     spaces[i,j] = val;
+                 else
+                     spaces[i,j] = -1;
+             
+             }
+         }
+         return spaces;
+     }
+ }
